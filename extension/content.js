82e8d6b2f4e19c54a559844c1a1828dad651eff6
Travis Saylor <travis.saylor@gmail.com>
2026-04-07 19:53:45 -0400

    [Chrome extension manifest and project scaffolding]

---
+++ extension/content.js
@@ -0,0 +1,2 @@
+// Content script for mac.bid pages
+// Placeholder — functionality will be added in US-003
---
+++ extension/service-worker.js
@@ -0,0 +1,2 @@
+// Background service worker
+// Placeholder — functionality will be added in later stories
